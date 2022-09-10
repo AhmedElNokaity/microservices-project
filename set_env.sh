97ab287c0ec0397c6576c6e3efdf52df0aa5ebfe
AhmedElNokaity <ahmedalnokity2013@gmail.com>
2022-09-10 19:02:51 +0200

    to MSs

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=postgrespass
+export POSTGRES_HOST=rds-postgres.ccz809gg73aq.eu-west-3.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
-export AWS_REGION=us-east-1
+export AWS_BUCKET=arn:aws:s3:::test-201508729060-dev-dmin
+export AWS_REGION=eu-west-3
 export AWS_PROFILE=default
 export JWT_SECRET=testing
 export URL=http://localhost:8100
